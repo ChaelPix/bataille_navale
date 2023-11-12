dee937bfad34676a97821696b3e53efa56071592
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-11-12 16:56:05 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Title");
+
+    sf::Event event;
+
+    while (window.isOpen()) {
+
+        while (window.pollEvent(event)) {
+
+            if (event.type == sf::Event::Closed) {
+
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::White);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            std::cout << "Hello world\n";
+
+        window.display();
+    }
+
+    return 0;
+}
