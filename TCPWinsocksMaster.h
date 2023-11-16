7105c40c5138a33ab0dd21f9bd00cb046a967da0
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-11-16 10:03:56 +0100

    Tcp client added

--- TCPWinsocksMaster.h
+++ TCPWinsocksMaster.h
@@ -1,11 +1,17 @@
 #pragma once
+#ifndef _TCPWinsock
+#define _TCPWinsock
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdexcept>
+#include <string>
 
 typedef unsigned int uint;
 typedef unsigned short ushort;
 typedef unsigned char uchar;
+
+const ushort DIMMAX = 2048;
 
 class TCPWinsocksMaster
 {
@@ -14,6 +20,8 @@ protected:
     SOCKET idSocket;
     struct sockaddr_in adr_serveur;
 
+    char trame_lect[DIMMAX + 1];
+
     void initializeWinsock();
     virtual void createSocket();
     void closeSocket();
@@ -21,12 +29,13 @@ protected:
 public:
 
     TCPWinsocksMaster();
-    virtual ~TCPWinsocksMaster();
+    ~TCPWinsocksMaster();
     virtual void init() = 0;
 
     const SOCKET getIdSocket();
 
-    void send(SOCKET targetSocketId);
-
-    void receive();
+    void sendMessage(SOCKET targetSocketId, std::string msg);
+    std::string receiveMessage();
 };
+
+#endif
