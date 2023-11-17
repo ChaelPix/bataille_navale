3aa426fde147d97717985205817b515553959afd
stein <raphaellaurent29@gmail.com>
2023-11-17 15:45:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 
-    sf::RenderWindow window(sf::VideoMode(600, 800), "BattleShip");
+    sf::RenderWindow window(sf::VideoMode(1300, 850), "BattleShip");
 
     sf::Texture shipTexture;
     if (!shipTexture.loadFromFile("ressources/UI/ui_boat_prototype.png")) {
@@ -17,25 +17,48 @@ int main()
     shipSprite.setTexture(shipTexture);
     shipSprite.setPosition(100, 100); // position initiale
 
-    // Dans la boucle de jeu
+    bool dragging = false;
+    sf::Sprite* selectedShip = nullptr;
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            // Gérer d'autres événements, comme les mouvements de la souris
+            // Gestion des événements de la souris
             if (event.type == sf::Event::MouseButtonPressed) {
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    // Mettre à jour la position du sprite de bateau, etc.
+                    // Vérifier si on clique sur un bateau
+                    if (shipSprite.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                        dragging = true;
+                        selectedShip = &shipSprite;
+                    }
+                }
+            }
+
+            if (event.type == sf::Event::MouseButtonReleased) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    dragging = false;
+                    selectedShip = nullptr;
+                    // Vérifiez si le bateau est dans une position valide et mettez à jour la grille
+                    // ...
+                }
+            }
+
+            if (event.type == sf::Event::MouseMoved) {
+                if (dragging && selectedShip != nullptr) {
+                    // Mettre à jour la position du bateau avec la souris
+                    selectedShip->setPosition(event.mouseMove.x, event.mouseMove.y);
                 }
             }
         }
 
         window.clear();
-        window.draw(shipSprite); // dessiner le sprite
+        window.draw(shipSprite);
         window.display();
     }
+
 
 
     return 0;
