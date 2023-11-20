a4ea0ba536504ab3264cbea77a1327227a1ab869
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-11-20 15:52:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,24 @@ int main() {
         TCPServer server(port);
 
         std::cout << "Serveur d\202marr\202 sur le port " << port << ". Appuyez sur Entr\202e pour arrêter..." << std::endl;
-        std::cin.get();
+        
+        Sleep(1000);
+        std::cout << "Cr\202ation Client 1 & 2" << std::endl;
+
+        TCPClient tcpClient1("10.187.52.31", 12345);
+        TCPClient tcpClient2("10.187.52.31", 12345);
+        Sleep(1000);
+        std::cout << tcpClient1.receiveMessage() << std::endl;
+        std::cout << tcpClient2.receiveMessage() << std::endl;
+        Sleep(1000);
+        tcpClient1.sendMessage("bonjour 1");
+        tcpClient2.sendMessage("ok");
+        Sleep(1000);
+        std::cout << tcpClient1.receiveMessage() << std::endl;
+        std::cout << tcpClient2.receiveMessage() << std::endl;
+        Sleep(1000);
+        tcpClient1.closeSocket();
+        tcpClient2.closeSocket();
 
         server.closeSocket();
         std::cout << "Serveur arret\202." << std::endl;
