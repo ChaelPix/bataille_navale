ba555b8d5d245c18ec283d39830719367f91edc5
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-12-03 16:13:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,54 @@
 
 #include "Application.h"
 
+void ServerMod()
+{
+    ushort port = 12345;
+
+    BattleshipServer server(port);
+    std::cout << "Serveur d\202marr\202 sur le port " << port << ". Appuyez sur Entr\202e pour arrêter..." << std::endl;
+
+    std::cin.get();
+    server.closeSocket();
+}
+
+void launchMenu()
+{
+    GameApplication game;
+    game.Run();
+}
+
+void launchGame()
+{
+    GameApplication game(GameApplication::State::Game);
+    game.Run();
+}
+
 int main() {
 
     std::cout << "[DEBUG] SELECT MENU: " << std::endl
         << "[0] Main Menu " << std::endl
-        << "[1] Game Menu " << std::endl;
+        << "[1] Game Menu " << std::endl
+        << "[2] Server Mod " << std::endl;
 
     char x;
     std::cin >> x;
 
-    if (x != '0')
+    switch (x)
     {
-        GameApplication game(GameApplication::State::Game);
-        game.Run();
-    }
-    else {
-        GameApplication game;
-        game.Run();
-    }
+        case '1':    
+            launchGame();
+            break;
+
+        case '2':
+            ServerMod();
+            break;
+
+        default:
+            launchMenu();
+            break;
+        }
+
 
     return 0;
 }
