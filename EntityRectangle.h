4b12b61de33933dcbd4837526c6895a55185cd92
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-12-06 20:47:50 +0100

    entity rectangle .h and .cpp splitted

--- EntityRectangle.h
+++ EntityRectangle.h
@@ -1,125 +1,38 @@
-#ifndef _EntityR
-#define _EntityR
+#ifndef _ENTITYRECTANGLE_H
+#define _ENTITYRECTANGLE_H
 
 #include "Entity.h"
-
+#include <SFML/Graphics.hpp>
+#include <string>
 
 class EntityRectangle : public Entity {
-
 protected:
     sf::RectangleShape shape;
     sf::Texture shapeTexture;
 
 public:
-    EntityRectangle(const sf::Vector2f& size) {
-        shape.setSize(size);
-    }
-    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position) {
-        shape.setSize(size);
-        shape.setPosition(position);
-    }
-    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, std::string texturePath) {
-        shape.setSize(size);
-        shape.setPosition(position);
-        shapeTexture.loadFromFile(texturePath);
-        setTexture(&(this->shapeTexture));
-    }
-    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, std::string texturePath, const sf::Vector2f& origin) {
-        shape.setSize(size);
-        shape.setPosition(position);
-        shapeTexture.loadFromFile(texturePath);
-        shape.setOrigin(origin);
-        setTexture(&(this->shapeTexture));
-    }
-    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, sf::Texture texture) {
-        shape.setSize(size);
-        shape.setPosition(position);
+    EntityRectangle(const sf::Vector2f& size);
+    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position);
+    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, std::string texturePath);
+    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, std::string texturePath, const sf::Vector2f& origin);
+    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, sf::Texture texture);
+    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, sf::Texture texture, sf::Color color);
+    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, sf::Texture texture, float angleDegres, const sf::Vector2f& origin);
 
-        shapeTexture = texture;
-        std::cout << "text load\n";
-        setTexture(&(this->shapeTexture));
-    }
-    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, sf::Texture texture, sf::Color color) {
-        shape.setSize(size);
-        shape.setPosition(position);
-        shape.setFillColor(color);
-        shapeTexture = texture;
-        setTexture(&(this->shapeTexture));
-    }
-    EntityRectangle(const sf::Vector2f& size, const sf::Vector2f& position, sf::Texture texture, float angleDegres, const sf::Vector2f& origin) {
-        shape.setSize(size);
-        shape.setPosition(position);
-        shape.setRotation(angleDegres);
-        shapeTexture = texture;
-        shape.setOrigin(origin);
-        setTexture(&(this->shapeTexture));
-    }
+    virtual void draw(sf::RenderWindow& window);
+    void update() override;
 
-    virtual void draw(sf::RenderWindow& window) {
-        window.draw(shape);
-    }
-
-    void update() override {
-        
-    }
-
-    void setTexture(sf::Texture* texture) {
-        shapeTexture = *texture;
-        shape.setTexture(texture);
-    }
-    
-    void setPosition(sf::Vector2f position)
-    {
-        shape.setPosition(position);
-    }
-
-    void setPosition(float x, float y)
-    {
-        shape.setPosition(sf::Vector2f(x, y));
-    }
-
-    sf::Vector2f getPosition()
-    {
-        return shape.getPosition();
-    }
-
-    void rotate(float angleTarget)
-    {
-        shape.setRotation(angleTarget);
-    }
-
-    void setOrigin(sf::Vector2f origin)
-    {
-        shape.setOrigin(origin);
-    }
-
-    void move(sf::Vector2f vel)
-    {
-        sf::Vector2f pos = shape.getPosition();
-        pos += vel;
-        shape.setPosition(pos);
-    }
-
-    void move(float velX, float velY)
-    {
-        sf::Vector2f pos = shape.getPosition();
-        pos += sf::Vector2f(velX, velY);
-        shape.setPosition(pos);
-    }
-
-    void setColor(sf::Color color)
-    {
-        shape.setFillColor(color);
-    }
-
-    sf::Color getColor() const {
-
-        return shape.getFillColor();
-    }
-
-    sf::RectangleShape getShape()
-    {
-        return shape;
-    }
+    void setTexture(sf::Texture* texture);
+    void setPosition(sf::Vector2f position);
+    void setPosition(float x, float y);
+    sf::Vector2f getPosition();
+    void rotate(float angleTarget);
+    void setOrigin(sf::Vector2f origin);
+    void move(sf::Vector2f vel);
+    void move(float velX, float velY);
+    void setColor(sf::Color color);
+    sf::Color getColor() const;
+    sf::RectangleShape getShape();
 };
+
 #endif
